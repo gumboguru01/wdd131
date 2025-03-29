5b376016c828972f1d7092d02ffc52c81cdc0823
gumboguru01 <ngumboguru@byupathway.edu>
2025-03-29 21:41:26 +0200

    initial commit

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -5,7 +5,7 @@ const temples = [
         dedicated: "2005, August, 7",
         area: 11500,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
     },
     {
         templeName: "Manti Utah",
@@ -13,7 +13,7 @@ const temples = [
         dedicated: "1888, May, 21",
         area: 74792,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
     },
     {
         templeName: "Payson Utah",
@@ -21,7 +21,7 @@ const temples = [
         dedicated: "2015, June, 7",
         area: 96630,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
     },
     {
         templeName: "Yigo Guam",
@@ -29,7 +29,7 @@ const temples = [
         dedicated: "2020, May, 2",
         area: 6861,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
     },
     {
         templeName: "Washington D.C.",
@@ -37,7 +37,7 @@ const temples = [
         dedicated: "1974, November, 19",
         area: 156558,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
     },
     {
         templeName: "Lima Perú",
@@ -45,7 +45,7 @@ const temples = [
         dedicated: "1986, January, 10",
         area: 9600,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
     },
     {
         templeName: "Mexico City Mexico",
@@ -53,47 +53,7 @@ const temples = [
         dedicated: "1983, December, 2",
         area: 116642,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
-    {
-        templeName: "Los Angeles California",
-        location: "Los Angeles, California, United States",
-        dedicated: "1956, March, 11",
-        area: 190614,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/los-angeles-california/los-angeles-california-temple-lds-1024x768.jpg",
-    },
-    {
-        templeName: "Oakland California",
-        location: "Oakland, California, United States",
-        dedicated: "1964, November, 17",
-        area: 80000,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/oakland-california/oakland-california-temple-lds-1024x768.jpg",
-    },
-    {
-        templeName: "San Diego California",
-        location: "San Diego, California, United States",
-        dedicated: "1993, April, 25",
-        area: 72000,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california/san-diego-california-temple-lds-1024x768.jpg",
-    },
-    {
-        templeName: "Fresno California",
-        location: "Fresno, California, United States",
-        dedicated: "2000, April, 9",
-        area: 10700,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/fresno-california/fresno-california-temple-lds-1024x768.jpg",
-    },
-    {
-        templeName: "Redlands California",
-        location: "Redlands, California, United States",
-        dedicated: "2003, September, 14",
-        area: 17000,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/redlands-california/redlands-california-temple-lds-1024x768.jpg",
-
-    },
+    // Add more temple objects here...
 ];
